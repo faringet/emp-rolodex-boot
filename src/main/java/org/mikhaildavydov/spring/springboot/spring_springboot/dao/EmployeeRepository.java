195aaaa6ef18327f351000ccc9631895b828f4ba
faringet <mik.davydov14@gmail.com>
2022-07-14 14:00:43 +0300

    add spring-boot-data-rest

--- src/main/java/org/mikhaildavydov/spring/springboot/spring_springboot/dao/EmployeeRepository.java
+++ src/main/java/org/mikhaildavydov/spring/springboot/spring_springboot/dao/EmployeeRepository.java
@@ -4,9 +4,11 @@ package org.mikhaildavydov.spring.springboot.spring_springboot.dao;
 
 import org.mikhaildavydov.spring.springboot.spring_springboot.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-    public List<Employee> findAllByName(String name);
+
 }
